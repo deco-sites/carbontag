19dd5b995d60ae61ac0ad2832ed0fdb61023477a
Leonardo Vizioli <viziolileonardo@gmail.com>
2024-12-07 19:29:38 +0000

    Publish 5 from viziolileonardo into origin/main

--- sections/Logos.tsx
+++ sections/Logos.tsx
@@ -5,6 +5,10 @@ export interface Logo {
   src?: ImageWidget;
   /** @description text alternative */
   altText?: string;
+  /** @description Width of the logo in pixels */
+  width?: number;
+  /** @description Height of the logo in pixels */
+  height?: number;
 }
 
 export interface Props {
@@ -16,6 +20,8 @@ const IMG_PLACEHODLER = Array(30).fill(0).map(() => ({
   src:
     "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/1527/03fbcc78-ca86-4616-a59a-b8aa18331a9c",
   altText: "Logo",
+  width: 200,
+  height: 100,
 }));
 
 export default function Logos({
@@ -29,9 +35,9 @@ export default function Logos({
           <Image
             src={logo.src || ""}
             alt={logo.altText || ""}
-            width={200}
-            height={200}
-            class="w-auto h-[100px] object-contain"
+            width={logo.width || 200}
+            height={logo.height || 100}
+            class="w-auto h-auto object-contain"
           />
         );
       })}
@@ -41,7 +47,7 @@ export default function Logos({
     <div class="lg:container md:max-w-6xl lg:mx-auto mx-4 py-6 lg:py-14">
       <div class="flex flex-col gap-14">
         <p class="text-center text-lg">{title}</p>
-        <div class="relative w-full overflow-hidden h-[100px]">
+        <div class="relative w-full overflow-hidden">
           <div class="animate-sliding absolute top-0 left-0 flex flex-nowrap">
             {slideContent}
           </div>
